098bc6243e9963d498bc96972c358c364208d7c1
Malak Gridah <malakgridah9@gmail.com>
2026-03-04 20:45:28 +0100

    added apps

---
+++ telecom-complains-contract-service/src/main/java/org/example/telecomcomplainscontractservice/TelecomComplainsContractServiceApplication.java
@@ -0,0 +1,12 @@
+package org.example.telecomcomplainscontractservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class TelecomComplainsContractServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomComplainsContractServiceApplication.class, args);
+
+    }
+
+}
---
+++ telecom-complains-contract-service/src/test/java/org/example/telecomcomplainscontractservice/TelecomComplainsContractServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.telecomcomplainscontractservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomComplainsContractServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
